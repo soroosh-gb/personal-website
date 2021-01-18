fbd8ca8731cfd0c3f76e811710a30e511a2cb381
soroosh-gb <soroosh.gb@gmail.com>
2021-01-17 19:25:09 -0500

    links

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../ProjectCard.css'
 import { Container, Row, Col, Image, Card, Button } from 'react-bootstrap';
 import projects from '../data'
-import WDM from '../assets/WDM.png'
-import Playlist from '../assets/Playlist.png'
-import SampleCache from '../assets/SampleCache.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 
 function ProjectCard(data) {
@@ -15,8 +15,8 @@ function ProjectCard(data) {
             <Card.Title>{data.data.name}</Card.Title>
             <Card.Img variant="top" src={data.data.image} />
             <Card.Body>
-                <Button></Button>
-                <Button></Button>
+            <a href={data.data.github} target="_blank"><FontAwesomeIcon icon={faGithub} size="4x" /></a>
+            <a href={data.data.video} target="_blank" className="Btn"><FontAwesomeIcon icon={faYoutube} size="4x" /></a>
             </Card.Body>
         </Card>
        
